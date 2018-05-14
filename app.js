ff90f49d273c37c8f66c421dcef512fd6c1d70c8
Enrique <etellezp@gmail.com>
2018-05-14 22:35:13 +0000

    Add template for campgrounds, link landing page with campgrounds

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get("/", function (req, res) {
   res.render("landing");
 });
 
+app.get("/campgrounds", function (req, res) {
+  var campgrounds = [
+      {name: "Salmon Creek", image: "http://photosforclass.com/download/pixabay-1845906?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104497f6c27aa3e9bcbe_960.jpg&user=Pexels"},
+      {name: "Granite Hill", image: "http://photosforclass.com/download/flickr-4684194306"},
+      {name: "Mountain Goat's Rest", image: "http://photosforclass.com/download/flickr-8444469474"}
+    ];
+    
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
 app.listen(process.env.PORT, process.env.IP, function () {
   console.log("The YelpCamp server has started!");
 });
