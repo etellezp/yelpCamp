df02023bc147dd850100b9a9fbbe0ad275404180
Enrique <etellezp@gmail.com>
2018-06-03 21:18:51 +0000

    Refactor app.js

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express   = require("express"),
+app = express(),
+bodyParser = require("body-parser"),
+mongoose = require("mongoose"),
+Campground = require("./models/campground")
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// SCHEMA SETUP
-
-var campgroundSchema = new mongoose.Schema ({
-  name: String,
-  image: String,
-  description: String 
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function (req, res) {
   res.render("landing");
