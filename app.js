168f8258b40d7f930ce8353f0a9e49d6f566f06b
Enrique <etellezp@gmail.com>
2018-06-05 13:53:51 +0000

    Nested routes to add comments to a particular campground, reference by Id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
 Campground = require("./models/campground"),
+Comment = require("./models/comment"),
 seedDB = require("./seeds")
 
 seedDB();
@@ -21,7 +22,7 @@ app.get("/campgrounds", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", {campgrounds:campgrounds});
+      res.render("campgrounds/index", {campgrounds:campgrounds});
     }
   });
 });
@@ -44,7 +45,7 @@ app.post("/campgrounds", function (req, res) {
 });
 
 app.get("/campgrounds/new", function (req, res) {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function (req, res) {
@@ -52,9 +53,42 @@ app.get("/campgrounds/:id", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("show", {campground: foundCampground});
+      res.render("campgrounds/show", {campground: foundCampground});
     }
   });
+});
+
+//========
+// COMMENTS
+//========
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      })
+    }
+  })
 });
 
 app.listen(process.env.PORT, process.env.IP, function () {
