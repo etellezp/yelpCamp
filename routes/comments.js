91a787d3a64c4a80543f3f5c9f2e43379e9dd7ad
Enrique <etellezp@gmail.com>
2018-06-07 16:25:37 +0000

    Association between user and comments

--- routes/comments.js
+++ routes/comments.js
@@ -23,6 +23,9 @@ router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
         if(err) {
           console.log(err);
         } else {
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          
           campground.comments.push(comment);
           campground.save();
           res.redirect("/campgrounds/" + campground._id);
